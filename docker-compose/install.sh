8ed606a9844766537c9bb9e2505c6df64931e1b6
root <root@open-hop-1.localdomain>
2022-08-14 23:36:18 +0800

    1

--- docker-compose/install.sh
+++ docker-compose/install.sh
@@ -1,7 +1,7 @@
 # 建议用迅雷下载二进制文件
-curl -L "https://kaosay.shop/docker/compose/releases/download/1.28.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose -k
+curl -L "https://kaosay.shop/docker/compose/releases/download/1.28.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/sbin/docker-compose -k
 # 下载的文件会输出到 /usr/local/bin/docker-compose
 
-sudo chmod +x /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/sbin/docker-compose
 
 docker-compose version
